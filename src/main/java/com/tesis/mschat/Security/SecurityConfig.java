79b64dc138bb70466b4e02de1faf8020d5a1bb84
andresveleze31 <andresvelezecheverry@gmail.com>
2025-11-12 10:05:44 -0500

    CORS

--- src/main/java/com/tesis/mschat/Security/SecurityConfig.java
+++ src/main/java/com/tesis/mschat/Security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4204", "http://192.168.0.135"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4204", "http://192.168.0.135", "https://firstepscol.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
